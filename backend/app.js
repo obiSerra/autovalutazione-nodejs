a5bff106d414790f42710b0226ac19336bd4057f
Roberto Serra <ext.roberto.serra@deltatre.it>
2021-03-03 23:19:10 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path")
+
+module.exports = (port) => {
+  const app = express();
+
+  app.use('/', express.static(path.join(__dirname, '../', 'dist')))
+  
+//   app.get("/", (req, res) => {
+//     express.static('public')
+//     res.send("Hello World!");
+//   });
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const runApp = require("./backend/app.js");
+
+const port = 3000
+
+runApp(port)
